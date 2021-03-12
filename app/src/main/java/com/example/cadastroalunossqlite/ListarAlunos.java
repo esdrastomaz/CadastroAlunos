9a16c95b2b8367d05ca9fd11e6420abc24e638a1
Esdras Tomaz <esdras_tomaz2@hotmail.com>
2021-03-12 01:09:46 -0300

    metodoconsultaraluno

--- app/src/main/java/com/example/cadastroalunossqlite/ListarAlunos.java
+++ app/src/main/java/com/example/cadastroalunossqlite/ListarAlunos.java
@@ -2,9 +2,14 @@ package com.example.cadastroalunossqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +30,55 @@ public class ListarAlunos extends AppCompatActivity {
         dao = new AlunoDAO(this);
         alunos = dao.obterTodos();
         alunosFiltrados.addAll(alunos);
-        ArrayAdapter<Aluno> adaptador = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        ArrayAdapter<Aluno> adaptador = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunosFiltrados);
         listView.setAdapter(adaptador);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_principal, menu);
+
+        SearchView sv = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                procuraAluno(s);
+                return false;
+            }
+        });
+
+
+        return true;
+    }
+
+    public void procuraAluno(String nome){
+        alunosFiltrados.clear();
+        for(Aluno a : alunos){
+            if(a.getNome().toLowerCase().contains(nome.toLowerCase())){
+                alunosFiltrados.add(a);
+            }
+        }
+
+        listView.invalidateViews();
+    }
+
+    public void cadastrar(MenuItem item){
+        Intent it =  new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        alunos = dao.obterTodos();
+        alunosFiltrados.clear();
+        alunosFiltrados.addAll(alunos);
+        listView.invalidateViews();
     }
 }
